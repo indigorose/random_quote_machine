75f8551a51bb79e9a6403473c889f73eff2b2e8c
indigorose <indigorose049@gmail.com>
2022-09-30 05:57:48 +0100

    Add color change function

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './quote.css';
 
 const Quote = () => {
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  // const color = document.querySelector('.color');
+
+  let hexColor = '#';
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  // color.textContent = hexColor;
+  // document.body.style.backgroundColor = hexColor;
+  console.log(hexColor);
+
+  function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+  }
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  // const [color, setColor] = useState(false);
+  // Need to sort out the background and the text.
+
   return (
     <>
       <div className="main-container">
@@ -10,7 +29,9 @@ const Quote = () => {
           <p id="author">author</p>
           <div className="container--quote-btn">
             <button id="tweet-quote">tweet icon</button>
-            <button id="new-quote">New Quote</button>
+            <button id="new-quote" onClick={handleClick}>
+              New Quote
+            </button>
           </div>
         </div>
       </div>
