907e2195d3568b420fa625e7ad45ae934eae00ca
indigorose <indigorose049@gmail.com>
2022-10-01 09:18:37 +0100

    Adding data quotes to the assets

---
+++ src/assets/data.js
@@ -0,0 +1,102 @@
+export const data = [
+  {
+    id: 1,
+    quote: 'No guts, no story.',
+    name: 'Chris Brady',
+  },
+  {
+    id: 2,
+    quote: "Screw it, let's do it.",
+    name: 'Richard Branson',
+  },
+  {
+    id: 3,
+    quote: 'Boldness be my friend.',
+    name: 'William Shakespeare',
+  },
+  {
+    id: 4,
+    quote: 'Dream big. Pray bigger.',
+    name: 'Anon',
+  },
+  {
+    id: 5,
+    quote: 'And so the adventure begins.',
+    name: 'Anon',
+  },
+  {
+    id: 6,
+    quote: 'If you want it, work for it.',
+    name: 'Anon',
+  },
+  {
+    id: 7,
+    quote: 'Broken crayons still colour.',
+    name: 'Anon',
+  },
+  {
+    id: 8,
+    quote: 'Grow through what you go through.',
+    name: 'Anon',
+  },
+  {
+    id: 9,
+    quote: 'Dream without fear. love without limits.',
+    name: 'Anon',
+  },
+  {
+    id: 10,
+    quote: 'She believed she could, so she did.',
+    name: 'Anon',
+  },
+  {
+    id: 11,
+    quote: 'Every noble work is at first impossible.',
+    name: 'Thomas Carlyle',
+  },
+  {
+    id: 12,
+    quote: 'We are twice armed if we fight with faith.',
+    name: 'Plato',
+  },
+  {
+    id: 13,
+    quote: 'Open your mind. Get up off the couch. Move.',
+    name: 'Anthony Bourdain',
+  },
+  {
+    id: 14,
+    quote: 'You matter.',
+    name: 'Anon',
+  },
+  {
+    id: 15,
+    quote: 'Fall seven times, stand up eight.',
+    name: 'Japanese proverb',
+  },
+  {
+    id: 16,
+    quote: 'Let him that would move the world first move himself.',
+    name: 'Socrates',
+  },
+  {
+    id: 17,
+    quote: 'Seek the seeker.',
+    name: 'Ramana Maharshi',
+  },
+  {
+    id: 18,
+    quote: 'Be a voice. Not an echo.',
+    name: 'Anon',
+  },
+  {
+    id: 19,
+    quote: 'Every wall is a door.',
+    name: 'Ralph Waldo Emerson',
+  },
+  {
+    id: 20,
+    quote: 'Silence is an answer too.',
+    name: 'Anon',
+  },
+];
